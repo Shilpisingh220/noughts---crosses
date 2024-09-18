d0052df1cad6df61807df1e5148688e357df6ddc
Shilpisingh220 <shilpibssingh27@gmail.com>
2024-09-18 14:19:24 +0000

    18 sep 2024

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,63 @@
-const resetBtn = document.querySelector(".btn-reset");
-const gameGrid = document.querySelector(".game-grid");
+// const fruits = ["apple", "kiwi", "banana", "mango", "cheeku"];
 
-const colors = ["orange", "blue", "red", "purple", "magenta", "yellow"];
+// const a = fruits.unshift();
 
-let count = 0;
+// console.log(a);
 
-resetBtn.addEventListener("click", function () {
-    console.log(count);
-    if (count > 5) count = count -6;
-    gameGrid.style.background = colors[count];
-    count = count + 1;
+// function getbetween(input) {
+//     return input.slice(1, input.lenght -1);
+// }
 
-});
+// const x = getbetween(fruits);
+// console.log(x);
+
+// console.log(getbetween([1, 2, 3, 4, 5]));
+
+
+// const fruits = ["apple", "kiwi", "banana", "mango", "cheeku"];
+
+// const z = fruits.join("-")
+
+// console.log(z);
+
+// function table(num) {
+//     for (let i = 1; i <= 10; i++){
+//         console.log(`${num}x${i}=${num * i}`);
+//     }
+
+// }
+
+// table(3);
+
+// const num = [1, 2, 3, 4,]
+// for (let j = 0; j < nums.length; j++) {
+//     nums[j] = num[j]*2
+// }
+
+// console.log(num)
+
+// const nums = [1, 20,32,443]
+// nums.forEach(function (v, idk) {
+//     nums[idk] = v * 2;
+
+// });
+
+// console.log(nums);
+
+const nums = [2,4,6,8]
+// numsEven = [flase,true,flase,true]
+
+// const evenNums = nums.map (function (value) {
+//     if (value % 2 == 0) {
+//         return "even";
+//     } else { 
+//         return "odd";
+//     }
+//     }
+    
+// });
+
+// console.log(evenNums);
+
+const evenNums = [];
+nums.forEach()
